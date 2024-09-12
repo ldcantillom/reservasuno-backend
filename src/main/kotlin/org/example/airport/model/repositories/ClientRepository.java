47e984acb1ebcf96c0ca5a0e020e9d57fa3092ad
JPA_project <ldcantillom@unimagdalena.edu.co>
2024-09-12 18:40:52 -0500

    [UPDATE] Avoiding Spanglish

--- src/main/kotlin/org/example/airport/model/repositories/ClientRepository.java
+++ src/main/kotlin/org/example/airport/model/repositories/ClientRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Collection;
 
-public interface ClienteRepository extends JpaRepository<Client,Long> {
+public interface ClientRepository extends JpaRepository<Client,Long> {
     List<Client> findByNombre(String nombre);
     List<Client> findByIdIn(Collection<Long> ids);
 }
