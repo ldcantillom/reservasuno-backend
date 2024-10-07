e5420e12841fa7fd7bff60e9b72e93f686f83c14
JPA_project <ldcantillom@unimagdalena.edu.co>
2024-10-07 15:59:20 -0500

    [UPDATE] using mapstruct library

--- src/main/kotlin/org/example/airport/model/mapper/ClientMapper.java
+++ src/main/kotlin/org/example/airport/model/mapper/ClientMapper.java
@@ -3,58 +3,16 @@ package org.example.airport.model.mapper;
 import org.example.airport.model.dtos.ClientDto;
 import org.example.airport.model.dtos.ClientIdDto;
 import org.example.airport.model.entities.Client;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class ClientMapper {
+@Mapper
+public interface ClientMapper {
 
-    public Client toEntity(ClientDto clientDto) {
-        Client client = new Client();
-        client.setFirstName(clientDto.firstname());
-        client.setLastName(clientDto.lastName());
-        client.setAddress(clientDto.address());
-        client.setCell(clientDto.cell());
-        client.setEmail(clientDto.email());
-        return client;
-    }
-
-    public ClientDto toDto(Client client) {
-        return new ClientDto(
-                client.getFirstName(),
-                client.getLastName(),
-                client.getAddress(),
-                client.getCell(),
-                client.getEmail()
-        );
-    }
-
-    public ClientIdDto toIdDto(Client client) {
-        return new ClientIdDto(
-                client.getId(),
-                client.getFirstName(),
-                client.getLastName(),
-                client.getAddress(),
-                client.getCell(),
-                client.getEmail()
-        );
-    }
-
-//    public List<ClientDto> toListDto(List<Client> clients) {
-//        List<ClientDto> clientDtos = new ArrayList<>();
-//        for (Client client : clients) {
-//            clientDtos.add(toDto(client));
-//        }
-//        return clientDtos;
-//    }
-
-    public List<ClientIdDto> toListIdDto(List<Client> clients) {
-        List<ClientIdDto> clientIdDtos = new ArrayList<>();
-        for (Client client : clients) {
-            clientIdDtos.add(toIdDto(client));
-        }
-        return clientIdDtos;
-    }
+    Client toEntity(ClientDto clientDto);
+    ClientDto toDto(Client client);
+    ClientIdDto toIdDto(Client client);
+    List<ClientIdDto> toListIdDto(List<Client> clients);
 }
--- src/main/kotlin/org/example/airport/model/mapper/PassengerMapper.java
+++ src/main/kotlin/org/example/airport/model/mapper/PassengerMapper.java
@@ -1,73 +1,20 @@
 package org.example.airport.model.mapper;
 
-import lombok.AllArgsConstructor;
 import org.example.airport.model.dtos.PassengerDto;
 import org.example.airport.model.dtos.PassengerIdDto;
 import org.example.airport.model.entities.Passenger;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-@AllArgsConstructor
-public class PassengerMapper {
-    private ReserveMapper reserveMapper;
-    public Passenger toEntity(PassengerDto passengerDto){
-        Passenger passenger = new Passenger();
-        passenger.setFirstName(passenger.getFirstName());
-        passenger.setLastName(passenger.getLastName());
-        passenger.setAddress(passenger.getAddress());
-        passenger.setCell(passenger.getCell());
-        passenger.setEmail(passenger.getEmail());
-        passenger.setIdentificationNumber(passenger.getIdentificationNumber());
-        passenger.setReserve(passenger.getReserve());
-        return passenger;
-    }
-    public PassengerDto toDto(Passenger passenger){
-        PassengerDto passangerDto = new PassengerDto(
-                passenger.getFirstName(),
-                passenger.getLastName(),
-                passenger.getAddress(),
-                passenger.getCell(),
-                passenger.getEmail(),
-                passenger.getIdentificationNumber(),
-                reserveMapper.toDto(passenger.getReserve())
-        );
-                return passangerDto;
-    }
-    public PassengerIdDto toIdDto(Passenger passenger){
-        PassengerIdDto passangerIdDto = new PassengerIdDto(
-                passenger.getId(),
-                passenger.getFirstName(),
-                passenger.getLastName(),
-                passenger.getAddress(),
-                passenger.getCell(),
-                passenger.getEmail(),
-                passenger.getIdentificationNumber(),
-                reserveMapper.toDto(passenger.getReserve())
-        );
-        return passangerIdDto;
-    }
-    public List<PassengerDto> toListDto(List<Passenger> passengers){
-        List<PassengerDto> passengerDtos = new ArrayList<>();
-        for(Passenger passenger : passengers){
-            passengerDtos.add(toDto(passenger));
-        }
-        return passengerDtos;
-    }
-    public List<PassengerIdDto> toListIdDto(List<Passenger> passengers){
-        List<PassengerIdDto> passengerIdDtos = new ArrayList<>();
-        for(Passenger passenger : passengers){
-            passengerIdDtos.add(toIdDto(passenger));
-        }
-        return passengerIdDtos;
-    }
-    public List<Passenger> toListEntity(List<PassengerDto> passengersDto){
-        List<Passenger> passengers = new ArrayList<>();
-        for(PassengerDto passengerDto : passengersDto){
-            passengers.add(toEntity(passengerDto));
-        }
-        return passengers;
-    }
+@Mapper
+public interface PassengerMapper {
+    PassengerMapper passengerMapperMapper = Mappers.getMapper(PassengerMapper.class);
+    Passenger toEntity(PassengerDto passengerDto);
+    PassengerDto toDto(Passenger passenger);
+    PassengerIdDto toIdDto(Passenger passenger);
+    List<PassengerDto> toListDto(List<Passenger> passengers);
+    List<PassengerIdDto> toListIdDto(List<Passenger> passengers);
+    List<Passenger> toListEntity(List<PassengerDto> passengersDto);
 }
