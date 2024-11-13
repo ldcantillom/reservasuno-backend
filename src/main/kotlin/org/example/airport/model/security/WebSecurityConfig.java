22bdf794d83a66cf35f60eee06e9d0b3bfddb7ad
León Cantillo <ldcantillom@unimagdalena.edu.co>
2024-11-13 16:11:44 -0500

    [FIX] Correction in the authentication path

--- src/main/kotlin/org/example/airport/model/security/WebSecurityConfig.java
+++ src/main/kotlin/org/example/airport/model/security/WebSecurityConfig.java
@@ -52,7 +52,7 @@ public class WebSecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unathorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 );
         http.authenticationProvider(authenticationProvider());
